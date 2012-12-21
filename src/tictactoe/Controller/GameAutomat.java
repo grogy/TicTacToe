736b1c68029bc058576c5c52f468fae750fa366b
Václav Makeš <info@makes.cz>
2012-12-21 01:03:03 +0100

    refaktorizace - zjednoduseni kodu

--- src/tictactoe/Controller/GameAutomat.java
+++ src/tictactoe/Controller/GameAutomat.java
@@ -4,12 +4,8 @@ import javax.swing.SwingUtilities;
 import tictactoe.GUI.Window;
 
 public class GameAutomat {
-	private StateApplication state;
+	private StateApplication state = StateApplication.START;
 
-	public GameAutomat() {
-		state = StateApplication.START;
-	}
-	
 	public void run() {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
